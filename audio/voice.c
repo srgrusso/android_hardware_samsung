f797aaec1cd98d85797385e8e05a0184c7baa025
followmsi <followmsi@gmail.com>
2018-04-02 00:02:22 +0200

    Revert "audio: support custom mixer-paths- and audience support routines"
    This reverts commit 2cafee27bcef44f1bc68b448ada837e46f78007f.

--- audio/voice.c
+++ audio/voice.c
@@ -34,14 +34,8 @@
 #include "audio_hw.h"
 #include "voice.h"
 
+#ifdef AUDIENCE_EARSMART_IC
 #include "audience.h"
-
-#ifndef AUDIENCE_SUPPORTED
-  #ifdef AUDIENCE_EARSMART_IC
-    #define AUDIENCE_SUPPORTED()  (true)
-  #else
-    #define AUDIENCE_SUPPORTED()  (false)
-  #endif
 #endif
 
 /**
@@ -288,10 +282,10 @@ int start_voice_session(struct voice_session *session)
         start_voice_session_bt_sco(session);
     }
 
-    if (AUDIENCE_SUPPORTED()) {
-        ALOGV("%s: Enabling Audience IC", __func__);
-        es_start_voice_session(session);
-    }
+#ifdef AUDIENCE_EARSMART_IC
+    ALOGV("%s: Enabling Audience IC", __func__);
+    es_start_voice_session(session);
+#endif
 
     if (session->two_mic_control) {
         ALOGV("%s: enabling two mic control", __func__);
@@ -336,10 +330,10 @@ void stop_voice_session(struct voice_session *session)
         stop_voice_session_bt_sco(session);
     }
 
-    if (AUDIENCE_SUPPORTED()) {
-        ALOGV("%s: Disabling Audience IC", __func__);
-        es_stop_voice_session();
-    }
+#ifdef AUDIENCE_EARSMART_IC
+    ALOGV("%s: Disabling Audience IC", __func__);
+    es_stop_voice_session();
+#endif
 
     session->out_device = AUDIO_DEVICE_NONE;
 
